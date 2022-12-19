3b3c7b39f7d51d64adda868a64fd6283d9178d97
Navid EMAD <navid.emad@yespark.fr>
2022-12-20 00:11:13 +0100

    Rework the way to build more generic

--- build.sh
+++ build.sh
@@ -1,5 +1,25 @@
 #!/usr/bin/env bash
-set -e
-docker build --no-cache --pull -t heroku-imagemagick container
-mkdir -p build
+set -x -e
+
+rm -rf build
+mkdir build
+
+DOCKER_FROM=heroku/heroku:22-build
+
+COMPILE_VERSION_LIBDE265=1.0.9
+COMPILE_VERSION_LIBWEBP=1.2.4
+COMPILE_VERSION_LIBHEIF=1.14.0
+COMPILE_VERSION_IMAGEMAGICK=7.1.0-55
+
+DOCKER_BUILDKIT=1 docker build \
+  --no-cache \
+  --tag heroku-imagemagick \
+  --pull \
+  --build-arg DOCKER_FROM=$DOCKER_FROM \
+  --build-arg COMPILE_VERSION_LIBDE265=$COMPILE_VERSION_LIBDE265 \
+  --build-arg COMPILE_VERSION_LIBWEBP=$COMPILE_VERSION_LIBWEBP \
+  --build-arg COMPILE_VERSION_LIBHEIF=$COMPILE_VERSION_LIBHEIF \
+  --build-arg COMPILE_VERSION_IMAGEMAGICK=$COMPILE_VERSION_IMAGEMAGICK \
+  container
+
 docker run --rm -t -v $PWD/build:/data heroku-imagemagick sh -c 'cp -f /usr/src/imagemagick/build/*.tar.bz2 /data/'
--- check.sh
+++ check.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-COMPILE_VERSION_LIBDE265=$(cat container/Dockerfile | egrep -o 'COMPILE_VERSION_LIBDE265=([0-9.-]+)' | cut -f 2 -d =)
-COMPILE_VERSION_LIBHEIF=$(cat container/Dockerfile | egrep -o 'COMPILE_VERSION_LIBHEIF=([0-9.-]+)' | cut -f 2 -d =)
-COMPILE_VERSION_LIBWEBP=$(cat container/Dockerfile | egrep -o 'COMPILE_VERSION_LIBWEBP=([0-9.-]+)' | cut -f 2 -d =)
-COMPILE_VERSION_IMAGEMAGICK=$(cat container/Dockerfile | egrep -o 'COMPILE_VERSION_IMAGEMAGICK=([0-9.-]+)' | cut -f 2 -d =)
+COMPILE_VERSION_LIBDE265=$(cat build.sh | egrep -o 'COMPILE_VERSION_LIBDE265=([0-9.-]+)' | cut -f 2 -d =)
+COMPILE_VERSION_LIBWEBP=$(cat build.sh | egrep -o 'COMPILE_VERSION_LIBWEBP=([0-9.-]+)' | cut -f 2 -d =)
+COMPILE_VERSION_LIBHEIF=$(cat build.sh | egrep -o 'COMPILE_VERSION_LIBHEIF=([0-9.-]+)' | cut -f 2 -d =)
+COMPILE_VERSION_IMAGEMAGICK=$(cat build.sh | egrep -o 'COMPILE_VERSION_IMAGEMAGICK=([0-9.-]+)' | cut -f 2 -d =)
 
 LATEST_VERSION_LIBDE265=$(curl -s "https://api.github.com/repos/strukturag/libde265/releases/latest" | jq -r .tag_name | sed 's/v//')
-LATEST_VERSION_LIBHEIF=$(curl -s "https://api.github.com/repos/strukturag/libheif/releases/latest" | jq -r .tag_name | sed 's/v//')
 LATEST_VERSION_LIBWEBP=$(curl -s "https://api.github.com/repos/webmproject/libwebp/tags" | jq -r '.[0].name' | sed 's/v//')
+LATEST_VERSION_LIBHEIF=$(curl -s "https://api.github.com/repos/strukturag/libheif/releases/latest" | jq -r .tag_name | sed 's/v//')
 LATEST_VERSION_IMAGEMAGICK=$(curl -s "https://api.github.com/repos/ImageMagick/ImageMagick/releases/latest" | jq -r .tag_name | sed 's/v//')
 
 if [ "$COMPILE_VERSION_LIBDE265" != "$LATEST_VERSION_LIBDE265" ]; then
